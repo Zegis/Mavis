735e7522b0c065cdf2e5bdabb71e7814fe36bc5e
Zegis <kjgm@poczta.onet.pl>
2016-01-20 01:28:14 +0100

    Fixed adding unecessary brackets in setFilter method

--- src/pl/kofun/mavis/LinesCounter.java
+++ src/pl/kofun/mavis/LinesCounter.java
@@ -89,7 +89,7 @@ public class LinesCounter{
 	
 	public void setFilter(String filterToApply){
 		if(!filterToApply.equals(filter))
-			filter = "(" +	filterToApply + ")";
+			filter = filterToApply;
 	}
 	
 	private void resetFile()
