901985ac8a7de9ab21ab65976bd3b3e624d7d813
Zegis <kjgm@poczta.onet.pl>
2016-09-04 00:54:53 +0200

    Added test json parsing

--- src/pl/kofun/mavis/trelloLite/ApacheHttpClient.java
+++ src/pl/kofun/mavis/trelloLite/ApacheHttpClient.java
@@ -3,6 +3,8 @@ package pl.kofun.mavis.trelloLite;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -10,11 +12,17 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
+import com.google.gson.Gson;
+
+import pl.kofun.mavis.trelloLite.Model.BoardList;
+
 public class ApacheHttpClient {
 
 	private CloseableHttpClient client;
 	private String key;
 	private String token;
+	
+	private static String baseUrl = "https://api.trello.com/1/";
 	
 	public ApacheHttpClient()
 	{
@@ -28,7 +36,7 @@ public class ApacheHttpClient {
 	}
 	
 	public void get(){		
-		String url = "https://api.trello.com/1//board/57bc03ee2d424a313f5f5d91/lists?fields=name,id&key="
+		String url = baseUrl + "board/57bc03ee2d424a313f5f5d91/lists?fields=name,id&key="
 				+ key + "&token=" + token;
 		
 		HttpGet get = new HttpGet(url);
@@ -37,11 +45,18 @@ public class ApacheHttpClient {
 		CloseableHttpResponse response1 = client.execute(get);
 		HttpEntity entity = response1.getEntity();
 		
-		String inputLine;
+		String json;
 		BufferedReader br = new BufferedReader(new InputStreamReader(entity.getContent()));
-		while((inputLine = br.readLine()) != null)
+		json = br.readLine();
+		Gson gson = new Gson();
+		if(json != null)
 		{
-				System.out.println(inputLine);
+			List<BoardList> lists = Arrays.asList(gson.fromJson(json, BoardList[].class));
+			for(BoardList list : lists)
+			{
+				if(list.getName().equals("TO DO"))
+					System.out.print(list.getName() + " " + list.getId() + "\n");
+			}
 		}
 		br.close();
 		
