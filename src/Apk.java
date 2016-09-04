3dbf9c5f7abdf6b5c7929369d961b8c291fb1b93
Zegis <kjgm@poczta.onet.pl>
2016-09-04 21:33:01 +0200

    Added main TrelloLite class. Proper functions to get lists and cards from given list

--- src/Apk.java
+++ src/Apk.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.List;
 import pl.kofun.mavis.DefaultTask;
 import pl.kofun.mavis.Library;
 import pl.kofun.mavis.MonthPlotter;
@@ -6,7 +7,8 @@ import pl.kofun.mavis.YearPlotter;
 import pl.kofun.mavis.Interfaces.MainTask;
 import pl.kofun.mavis.Options;
 
-import pl.kofun.mavis.trelloLite.ApacheHttpClient;;
+import pl.kofun.mavis.trelloLite.TrelloLite;
+import pl.kofun.mavis.trelloLite.Model.BoardList;
 
 public class Apk{
 
@@ -15,9 +17,20 @@ public class Apk{
 		MainTask task;
 		Options options = new Options(args);			
 		
-		ApacheHttpClient client = new ApacheHttpClient();
-		client.configure(options.get("apiKey"), options.get("apiToken"));
-		client.get();
+//		ApacheHttpClient client = new ApacheHttpClient();
+//		client.configure(options.get("apiKey"), options.get("apiToken"));
+//		client.get();
+		
+		TrelloLite trello = new TrelloLite(options.get("apiKey"),options.get("apiToken"));
+		List<BoardList> boardLists = trello.getListsFromBoard();
+		
+		for(BoardList list : boardLists)
+		{
+			int count = trello.getCardsFromList(list.getId()).size();
+			
+			System.out.println(list.getName()+ ":");
+			System.out.println("Has " + count + " cards");
+		}
 		
 		if(options.containsKey("Task"))
 		{	
--- src/pl/kofun/mavis/trelloLite/ApacheHttpClient.java
+++ src/pl/kofun/mavis/trelloLite/ApacheHttpClient.java
@@ -11,63 +11,39 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
-import com.google.gson.Gson;
-
-import pl.kofun.mavis.trelloLite.Model.BoardList;
-
-public class ApacheHttpClient {
+class ApacheHttpClient {
 
 	private CloseableHttpClient client;
-	private String key;
-	private String token;
 	
-	private static String baseUrl = "https://api.trello.com/1/";
 	
 	public ApacheHttpClient()
 	{
 		client = HttpClients.createMinimal();
 	}
 	
-	public void configure(String key, String token)
-	{
-		this.key = key;
-		this.token = token;
-	}
-	
-	public void get(){		
-		String url = baseUrl + "board/57bc03ee2d424a313f5f5d91/lists?fields=name,id&key="
-				+ key + "&token=" + token;
+	public String get(String url){		
 		
+		String ret;
 		HttpGet get = new HttpGet(url);
 		try
 		{
 		CloseableHttpResponse response1 = client.execute(get);
 		HttpEntity entity = response1.getEntity();
 		
-		String json;
-		BufferedReader br = new BufferedReader(new InputStreamReader(entity.getContent()));
-		json = br.readLine();
-		Gson gson = new Gson();
-		if(json != null)
-		{
-			List<BoardList> lists = Arrays.asList(gson.fromJson(json, BoardList[].class));
-			for(BoardList list : lists)
-			{
-				if(list.getName().equals("TO DO"))
-					System.out.print(list.getName() + " " + list.getId() + "\n");
-			}
-		}
-		br.close();
+		ret = EntityUtils.toString(entity); 
 		
 		}
 		catch(Exception e)
 		{
-			
+			ret = "";
 		}
 		finally{
 			get.releaseConnection();
 		}
+		
+		return ret;
 	}
 	
 	public void finalize()
