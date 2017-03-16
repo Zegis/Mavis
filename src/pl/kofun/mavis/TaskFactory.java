ab79efd51a4e3de8e058be1a28b064fa4215f5b6
Zegis <kjgm@poczta.onet.pl>
2017-03-16 23:39:39 +0100

    Added projects module wireframe

--- src/pl/kofun/mavis/TaskFactory.java
+++ src/pl/kofun/mavis/TaskFactory.java
@@ -21,6 +21,10 @@ public class TaskFactory {
 			{
 				return new YearPlotter(options);
 			}
+			else if (options.get("Task").equals("Ps"))
+			{
+				return new Projects(); 
+			}
 		}
 		
 		return new DefaultTask();
