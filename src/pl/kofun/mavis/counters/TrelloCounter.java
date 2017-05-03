a05406129fe039a8b867ce79b47649523b1cfa26
Zegis <kjgm@poczta.onet.pl>
2017-05-03 17:13:24 +0200

    Some comments

--- src/pl/kofun/mavis/counters/TrelloCounter.java
+++ src/pl/kofun/mavis/counters/TrelloCounter.java
@@ -19,12 +19,17 @@ public class TrelloCounter implements Counter {
 	public TrelloCounter(String key, String token, String filter)
 	{
 		client = new TrelloLite(key, token);
+		// We need to trim filter, as typical filter has parenthesis
 		this.filter = filter.substring(1, filter.length()-1);
 	}
 	
 	public void setFilter(String newFilter)
 	{
-		this.filter = newFilter.substring(1,newFilter.length()-1);
+		if(!newFilter.equals(filter))
+		{
+			// We need to trim filter, as typical filter has parenthesis
+			this.filter = newFilter.substring(1,newFilter.length()-1);
+		}
 	}
 	
 	@Override
